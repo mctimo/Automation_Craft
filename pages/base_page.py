75eb11e5f4dd6cfc381f971d0e4a8b746f1531c5
Artem.Ostapenko <timfenomen@gmail.com>
2023-04-08 00:38:18 +0400

    fix elements_is_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -17,10 +17,10 @@ class BasePage:
         return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     def element_is_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
     def element_are_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     def element_is_clickable(self, locator, timeout=5):
         return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
