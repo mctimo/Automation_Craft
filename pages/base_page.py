eba0d169f1047479dfdd5e855d16a42da83bf3db
Artem.Ostapenko <timfenomen@gmail.com>
2023-04-08 00:18:43 +0400

    base + elements page

---
+++ pages/base_page.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_are_present(self, locator, timeout=5):
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("argument[0].scrollIntoView();", element)
