ed32213c1e550f0a0454fb4a0e69b001e117aae2
Artem.Ostapenko <timfenomen@gmail.com>
2023-05-01 00:07:11 +0400

    some changes

--- generator/generator.py
+++ generator/generator.py
@@ -3,10 +3,12 @@ from faker import Faker
 
 faker_ru = Faker('ru_RU')
 Faker.seed()
+
+
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + ' ' + faker_ru.last_name() + ' ' + faker_ru.middle_name(),
-        email=faker_ru.email,
+        email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address()
     )
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,9 +10,8 @@ class TestElements():
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
-            print(email)
             out_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_form()
-            assert full_name == out_name
-            assert email == out_email
-            assert current_address == out_current_address
-            assert permanent_address == out_permanent_address
+            assert full_name == out_name, "Names don't match"
+            assert email == out_email, "Emails don't match"
+            assert current_address == out_current_address, "Current addresses don't match"
+            assert permanent_address == out_permanent_address, "Permanent addresses don't match"
